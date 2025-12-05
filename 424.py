0a4067b1029c15a347ef7f86d26f7b202c4df636
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-05 16:26:30 +0800

    while loop sol

--- 424.py
+++ 424.py
@@ -3,25 +3,35 @@ import time
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        left, right, ans= 0,0,0
-        map: dict[str, int] =  dict()
-        targ = s[0]
-        while left <= right < len(s):
-            char = s[right]
-            if (char not in map):
-                map[char] = 1
-            else:
-                map[char] += 1
-            if map[char] > map[targ]:
-                targ = char
-            length = right+1-left
-            if length - map[targ] > k:
-                map[s[left]] -= 1
-                left += 1
-            else:
-                ans = max(ans, length)
-            right += 1
+        l, ans, maxf = 0,0,0
+        map: dict[str,int] = dict()
+        for r in range(len(s)):
+            map[s[r]] = 1 + map.get(s[r], 0)
+            maxf = max(maxf, map[s[r]])
+            while ((r-l+1) - maxf > k):
+                map[s[l]] -=1
+                l+=1
+            ans = max(ans, r-l+1)
         return ans
+        # left, right, ans= 0,0,0
+        # map: dict[str, int] =  dict()
+        # targ = s[0]
+        # while left <= right < len(s):
+        #     char = s[right]
+        #     if (char not in map):
+        #         map[char] = 1
+        #     else:
+        #         map[char] += 1
+        #     if map[char] > map[targ]:
+        #         targ = char
+        #     length = right+1-left
+        #     if length - map[targ] > k:
+        #         map[s[left]] -= 1
+        #         left += 1
+        #     else:
+        #         ans = max(ans, length)
+        #     right += 1
+        # return ans
 
 
 test = Solution()
