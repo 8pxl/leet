a869530c572bfe58877f88928c49ee3ef070d552
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-08 14:28:44 +0800

    add print mehod

--- lib.py
+++ lib.py
@@ -1,9 +1,15 @@
 class ListNode:
-    def __init__(self, val: int = 0, next: ListNode | None =None):
+    def __init__(self, val: int = 0, next: ListNode | None = None):
         self.val: int = val
         self.next: ListNode | None = next
+    def print(self):
+        print(self.val)
+        if self.next:
+            self.next.print()
 
-def build_linked_list(values: list[int]) -> ListNode:
+type Node = ListNode | None
+
+def arrtoll(values: list[int]) -> ListNode:
     head = ListNode(values[0])
     current = head
     for v in values[1:]:
