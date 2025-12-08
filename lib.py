47caebf66cdb84b7e7e315c89390520b68e8a6f0
8pxl <56054380+8pxl@users.noreply.github.com>
2025-12-08 18:02:55 +0800

    add str method

--- lib.py
+++ lib.py
@@ -1,11 +1,18 @@
+from typing import override
+
+
 class ListNode:
     def __init__(self, val: int = 0, next: ListNode | None = None):
         self.val: int = val
         self.next: ListNode | None = next
+    @override
+    def __str__(self):
+        return str(self.val)
     def print(self):
         print(self.val)
         if self.next:
             self.next.print()
+
 
 type Node = ListNode | None
 
